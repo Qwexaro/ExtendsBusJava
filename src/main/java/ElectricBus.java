7c826a160da9da7638de6982b03a1c079a6940be
Qwexaro <Sergikstar25.gold@mail.ru>
2025-04-01 14:17:42 +0300

    main

--- src/main/java/ElectricBus.java
+++ src/main/java/ElectricBus.java
@@ -1,8 +1,21 @@
-public class ElectricBus extends Bus{
+public class ElectricBus extends Bus {
     private final double minimalTankFullnessRate;
 
     public ElectricBus(double fuelConsumption, double minimalTankFullnessRate) {
         super(fuelConsumption);
         this.minimalTankFullnessRate = minimalTankFullnessRate;
+    }
+
+    public boolean run(int distance) {
+        if (drivingDistance() < distance) {
+            return false;
+        }
+
+        if (tankFullnessRate < minimalTankFullnessRate) {
+            return false;
+        }
+
+        tankFullnessRate -= fuelConsumption * distance;
+        return true;
     }
 }
